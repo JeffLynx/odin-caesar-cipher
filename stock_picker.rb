880c637eb1b893d5cc59e4dbffbfd17a4c10321b
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-09-26 16:04:10 +0200

    fixed the obvious problem that you could buy after you sell duh

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,21 +1,27 @@
-#Each index of the input array prices contains the price of some good on one day.
-#Days start at 0.
-
-#example_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
-
-#The method returns an array containing
-#the best day to buy(lowest price) 
-#and the best day to sell(highest price)
-
 def stock_picker(prices)
-  #buy = find the index with lowest number
-  buy = prices.index(prices.min())
-  #sell = find the index with the highest number
-  sell = prices.index(prices.max())
-  #Return an array conaing the best days to but and sell
-  #[lowest, highest]
-  [buy, sell]
+  #initialize variables to keep track of the best profit, buy day and sell day found
+  best_profit, best_buy_day, best_sell_day = 0, 0, 0
+  
+  #initialize variables to keep track of the earliest day with the best price found
+  lowest_price_so_far, lowest_day_so_far = prices[0], 0
+  
+  #iterate
+  prices.each_with_index do |price, index|
+    
+    #check the profit of the current day
+    profit_today = price - lowest_price_so_far
+    
+    #update the best profit, buy day and sell day if the current day beats any of the previous
+    if profit_today > best_profit
+      best_profit, best_buy_day, best_sell_day = profit_today, lowest_day_so_far, index
+    end
+    
+    #update the value of the day with the lowest price if the current day beats any of the previous 
+    if price < lowest_price_so_far
+      lowest_price_so_far, lowest_day_so_far = price, index
+    end
+  end
+  
+  #return the best day to buy and the best day to sell
+  [best_buy_day, best_sell_day]
 end
-
-#stock_picker(example_prices)
-#=> [7, 0]
